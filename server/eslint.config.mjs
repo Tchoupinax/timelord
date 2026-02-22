5075e8237426e9f4bcd52a57a0a78372280ba1d7
Tchoupinax <corentinfiloche@hotmail.fr>
2026-02-22 10:36:11 +0100

    fix rules

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -4,17 +4,6 @@ import { eslintTypescript } from "eslint-config-tchoupinax";
 export default [
   ...eslintTypescript,
   {
-    ignores: [
-      "prisma/generated/**",
-    ],
-  },
-  {
-    rules: {
-      "@stylistic/operator-linebreak": [
-        "error",
-        "after",
-        { overrides: { "+=": "before", "|": "before", "?": "before", ":": "before" } },
-      ],
-    },
+    ignores: ["prisma/generated/**"],
   },
 ];
